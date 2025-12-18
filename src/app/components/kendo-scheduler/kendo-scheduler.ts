e396dba292148f8dd04de7b095915e33bd98fbbe
bkindzierski@merchantsgroup.com <bkindzierski@merchantsgroup.com>
2025-12-18 09:56:50 -0500

    scheduler updates

--- src/app/components/kendo-scheduler/kendo-scheduler.ts
+++ src/app/components/kendo-scheduler/kendo-scheduler.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { KENDO_SCHEDULER, SchedulerEvent } from '@progress/kendo-angular-scheduler';
+import { EventStyleArgs, KENDO_SCHEDULER, SchedulerEvent } from '@progress/kendo-angular-scheduler';
 
 
 @Component({
   selector: 'my-kendo-scheduler',
   imports: [KENDO_SCHEDULER, FormsModule],
   templateUrl: './kendo-scheduler.html',
-  styleUrl: './kendo-scheduler.css',
+  styleUrl: './kendo-scheduler.scss',
 })
 export class MyKendoScheduler implements OnInit {
 
@@ -20,25 +20,25 @@ export class MyKendoScheduler implements OnInit {
   public events: SchedulerEvent[] = [
         {
             id: 1,
-            title: 'JMK-do not schedule',
+            title: '1 JMK-do not schedule',
             start: new Date('2025-12-12T09:00:00'),
             end: new Date('2025-12-12T10:00:00')
         },
         {
             id: 2,
-            title: 'BDK-do not schedule',
+            title: '2 BDK-do not schedule',
             start: new Date('2025-12-18:00:00'),
             end: new Date('2025-12-18:00:00')
         },
         {
             id: 3,
-            title: 'BDK-do not schedule',
+            title: '3 BDK-do not schedule',
             start: new Date('2025-12-31:00:00'),
             end: new Date('2025-12-31:00:00')
         },
         {
             id: 4,
-            title: 'JMK-do not schedule',
+            title: '4 JMK-do not schedule',
             start: new Date('2025-12-26:00:00'),
             end: new Date('2025-12-26:00:00')
         }
@@ -50,4 +50,11 @@ export class MyKendoScheduler implements OnInit {
   ngOnInit(){
 
   }
+  public getEventClass = (args: EventStyleArgs) => {
+    
+    const eventId = args.event.dataItem.id;    
+    if (eventId === 1 || eventId === 4) return "high-priority";
+    if (eventId === 2 || eventId === 5) return "medium-priority";
+    return "low-priority";
+  };
 }
