0c3f7125e1a1527a4ea4a25fdd6ace51f83f7bdf
bkindzierski@merchantsgroup.com <bkindzierski@merchantsgroup.com>
2026-01-09 11:48:12 -0500

    added kendo login components

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonFillMode, ButtonRounded, ButtonSize, ButtonThemeColor, KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
+import { loginIcon, SVGIcon } from '@progress/kendo-svg-icons';
+import { Subscription } from 'rxjs';
+import { KENDO_INPUTS,InputFillMode,  InputRounded,  InputSize } from "@progress/kendo-angular-inputs";
+import { KENDO_LABELS } from "@progress/kendo-angular-label";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,KENDO_BUTTONS,KENDO_INPUTS,KENDO_LABELS],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
 form: FormGroup
+formChangesSubscription!: Subscription;
+//
+public size: ButtonSize = "medium";
+public rounded: ButtonRounded = "medium";
+public fillMode: ButtonFillMode = "outline";
+public themeColor: ButtonThemeColor = "primary";
+public svgData: SVGIcon = loginIcon;
+//textboxes
+public tbfillMode: InputFillMode = "outline";
 
 constructor(){
 
@@ -24,8 +38,13 @@ ngOnInit(): void {
       password: new FormControl(''),
       //email: new FormControl('')
     });
-  }
-  
+
+    // this.formChangesSubscription = this.form.valueChanges.subscribe(changes => {
+    //   console.log('Form value changed:', changes);
+    //   // You can implement custom logic here, like saving data or updating the UI
+    // });
+}
+
  onSubmit(): void{
   console.log('Form Submitted!', this.form.value)
  }
